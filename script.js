272a0631fd461b5d7bb1c9d69b058ffa63493a75
Stefano <stefano.zaccaria29@gmail.com>
2020-07-27 12:59:40 +0200

    milestone 1 complete

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 function init() {
-    var input = $('.container #search');
-    var btn = $('.container #btn');
-    // btn.click(function () {
-    //     var inputVal = input.val(); 
-    //     console.log(inputVal);
-    // });
-    btn.click(searchApi);
+    searchApi();
 }
 
 function searchApi() {
-    var input = $('.container>#search');
-    var inputVal = input.val();
-   $.ajax({
-    url : 'api.themoviedb.org/3/search/movie?',
-    method : 'GET',
-    data : {
-        'api_key': 'aebf9ba0680152a5c118e16606ba7947',
-        'query': 'ritorno al futuro'
-    },
-    success: function (data) {
+    var input = $('.container #search');
+    var btn = $('.container #btn');
+    btn.click(function () {
+        var inputVal = input.val(); 
+    
+        $.ajax({
+            url : 'https://api.themoviedb.org/3/search/movie?',
+            method : 'GET',
+            data : {
+                'api_key': 'aebf9ba0680152a5c118e16606ba7947',
+                'query': inputVal
+            },
+            success: function (data) {
 
-        var response = data.response;
-        var success = data.success;
+                var results = data.results;
+                var template = $('#movie-template').html();
+                var compiled = Handlebars.compile(template);
+                var target = $('#movie-list');
 
-        console.log(data);
-        console.log(response);
-        console.log(success);
-        
-    },
-    error: function (error) {
-        console.log(error);
-    }
+                for (var i = 0; i < results.length; i++) {
+                    var movieTitle = results[i].title;
+                    var movieOriginalTitle = results[i]['original_title'];
+                    var movielenguage = results[i]['original_language'];
+                    var movieVote = results[i]['vote_average'];
 
-   });
+                    var movieHTML = compiled({
+                        'title': movieTitle,
+                        'titleOriginal' : movieOriginalTitle,
+                        'lenguage' : movielenguage,
+                        'vote': movieVote
+                    });
+                    
+                    target.append(movieHTML);
+                }
+                
+            },
+            error: function (error) {
+                console.log(error);
+            }
+
+        });
+    });
 }
 
 $(document).ready(init);
