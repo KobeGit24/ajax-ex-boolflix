4c2822d2f6dd3e722c1791a5695237e2bd0ac161
Stefano <stefano.zaccaria29@gmail.com>
2020-07-29 11:09:36 +0200

    milestone complete

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function searchApi() {
 }
 
 function pressApi(event) {   
-    if (event.which==13) {
+    if (event.which==13 || event.keyCode==13) {
         callApi(); 
     }
 }
@@ -22,82 +22,8 @@ function callApi() {
     var target = $('#movie-list');
     $('.movie-wall h1').hide();
     target.html('');
-
-    $.ajax({
-        url : 'https://api.themoviedb.org/3/search/movie?',
-        method : 'GET',
-        data : {
-            'api_key': 'aebf9ba0680152a5c118e16606ba7947',
-            'query': inputVal
-        },
-        success: function (data) {
-
-            var results = data.results;
-            var template = $('#movie-template').html();
-            var compiled = Handlebars.compile(template);
-            var flags =['de','en','es','fr','it','us','ja','hi','zh'];
-            
-            for (var i = 0; i < results.length; i++) {
-
-                var result = results[i];
-                var vote = result.vote_average;
-                var lenguage = result.original_language;
-
-                result.stars = starsVote(vote);
-                result.type = 'Movie';
-
-                if (flags.includes(lenguage)) {
-                    result.flag = lenguage;
-                }
-
-                var movieHTML = compiled(result);
-                target.append(movieHTML);
-            }
-            
-        },
-        error: function (error) {
-            console.log(error);
-        }
-
-    });
-
-    $.ajax({
-        url : 'https://api.themoviedb.org/3/search/tv?',
-        method : 'GET',
-        data : {
-            'api_key': 'aebf9ba0680152a5c118e16606ba7947',
-            'query': inputVal
-        },
-        success: function (data) {
-
-            var results = data.results;
-            var template = $('#movie-template').html();
-            var compiled = Handlebars.compile(template);
-            var flags =['de','en','es','fr','it','us'];
-            
-            for (var i = 0; i < results.length; i++) {
-
-                var result = results[i];
-                var vote = result.vote_average;
-                var lenguage = result.original_language;
-
-                result.stars = starsVote(vote);
-                result.type = 'Serie';
-                
-                if (flags.includes(lenguage)) {
-                    result.flag = lenguage;
-                }
-
-                var seriesHTML = compiled(result);
-                target.append(seriesHTML);
-            }
-            
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    });
-    
+    apiOrganize('movie', inputVal);
+    apiOrganize('tv', inputVal);   
 }
 
 function starsVote (vote) {
@@ -124,6 +50,53 @@ function infoOnHover() {
         $(this).children('#img').slideToggle('slow');
     });
     
+}
+
+function apiOrganize(type,inputVal) {
+
+    $.ajax({
+        url : `https://api.themoviedb.org/3/search/${type}`,
+        method : 'GET',
+        data : {
+            'api_key': 'aebf9ba0680152a5c118e16606ba7947',
+            'query': inputVal
+        },
+        success: function (data) {
+
+            var results = data.results;
+            var target = $('#movie-list');
+            var template = $('#movie-template').html();
+            var compiled = Handlebars.compile(template);
+            var flags =['de','en','es','fr','it','us','ja','hi','zh'];
+            
+            for (var i = 0; i < results.length; i++) {
+
+                var result = results[i];
+                var vote = result.vote_average;
+                var lenguage = result.original_language;
+
+                result.stars = starsVote(vote);
+
+                if (type == 'movie') {
+                    result.type = 'Movie';  
+                } else if (type == 'tv') {
+                    result.type = 'Serie';
+                }
+
+                if (flags.includes(lenguage)) {
+                    result.flag = lenguage;
+                }
+
+                var cardHTML = compiled(result);
+                target.append(cardHTML);
+            }
+            
+        },
+        error: function (error) {
+            console.log(error);
+        }
+
+    });
 }
 
 $(document).ready(init);
